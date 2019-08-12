64014fc75cf99ce78df145331bdeaa0f99434c2c
Leandro Acquaroli <26485264+lnacquaroli@users.noreply.github.com>
2019-08-12 14:45:21 -0400

    Update ThinFilmsTools.jl

--- src/ThinFilmsTools.jl
+++ src/ThinFilmsTools.jl
@@ -11,12 +11,9 @@ export LayerTOM, HeaterGeometry, Source, ThreeOmegaMethod
 
 using Plots, LaTeXStrings # To use with PlottingTools.jl
 pyplot(grid=false)
-#gr(size=(600,450), grid=false, format=:svg)
 include("PlottingTools.jl")
 export TOMplot, TMMOplotSpectra1D, TMMOplotSpectraAngle1D, TMMOplotSpectra2D, TMMOplotEMF2D, TMMOplotEMFAngle2D, TMMOplotNprofile, TMMOplotdispersion
 
-# datapath = joinpath(@__DIR__, "data/")
-# include(datapath * "RIdb.jl")
 include("RefractiveIndicesDB.jl")
 export RIdb
 
