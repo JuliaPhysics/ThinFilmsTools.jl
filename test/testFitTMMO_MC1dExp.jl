4fbb54e0220981b6d085636fd038d503723b2bfa
Leandro Acquaroli <26485264+lnacquaroli@users.noreply.github.com>
2019-09-20 14:42:31 -0400

    Add files via upload

---
+++ test/testFitTMMO_MC1dExp.jl
@@ -0,0 +1,28 @@
+using Optim
+
+λ = 400:1000
+θ = [5.]
+pol = 0.5
+beam = PlaneWave(λ, θ; p=pol)
+
+n1 = RIdb.air(beam.λ)
+n2 = RIdb.silicon(beam.λ)
+n3 = RIdb.glass(beam.λ)
+
+layers = [ LayerTMMO1DIso(n1),
+           ModelFit(:looyengaspheresbin; N=(n1, n2)),
+           ModelFit(:looyengaspheresbin; N=(n1, n2)),
+           LayerTMMO1DIso(n3) ]
+
+Rexp = Utils.getFPSpectrum(beam.λ)
+
+options = Optim.Options(g_abstol=1e-8, g_reltol=1e-8, iterations=10^6, show_trace=true, store_trace=true);
+
+seed = [[119.0, 0.8], [76.0, 0.5]]
+solOptim = FitTMMO1DIsotropic(Reflectance(), seed, beam, Rexp, layers; arrange=MC1d(), L=[4, 4], Ld=[2], options=options, alg=SAMIN(), lb=0.5.*seed, ub=1.5.*seed)
+solOptim2 = FitTMMO1DIsotropic(Reflectance(), seed, beam, Rexp, layers; arrange=MC1d(), L=[4, 4], Ld=[2], options=options, alg=NelderMead())
+
+seed2 = [[120, 0.8], [83.0, 0.5], [0.995]]
+solOptim3 = FitTMMO1DIsotropic(Reflectance(), seed2, beam, Rexp, layers; arrange=MC1dAlpha(), L=[4, 4], Ld=[2], options=options, alg=SAMIN(), lb=0.5.*seed2, ub=1.5.*seed2)
+solOptim4 = FitTMMO1DIsotropic(Reflectance(), seed2, beam, Rexp, layers; arrange=MC1dAlpha(), L=[4, 4], Ld=[2], options=options, alg=NelderMead())
+
--- test/testTMMO.jl
+++ test/testTMMO.jl
@@ -1,21 +1,22 @@
-function dummy(λ::AbstractArray{T,N}, x::S, y::S) where {T<:Number, N, S<:Number}
-    Nc = ones.(length(λ)) .* (x + im*y)
-end # EOF dummy(...)
-
 # Define beam
-λi = 400 # intial wavelength [nm]
-λf = 1000 # final wavelength [nm]
-λ = LinRange(λi, λf, λf-λi+1) # wavelength range [nm]
+λi = 200 # intial wavelength [nm]
+λf = 5000 # final wavelength [nm]
+λ = LinRange(λi, λf, 1000) # wavelength range [nm]
 λ0 = 700. # reference wavelength
-θ = [0.] # angle of incidence [degrees]
-beam = PlaneWave(λ, λ0, θ)
+θi = 0
+θf = 90
+θ = LinRange(0, 90, θf-θi)
+beam = PlaneWave(λ, θ)
 
 # Define layers
-l0 = LayerTMMO1DIso(type=:GT, n=dummy(beam.λ, 1., 0.), d=0.)
-l1 = LayerTMMO1DIso(type=:GT, n=dummy(beam.λ, 2.5, 0.5), d=150.)
-l2 = LayerTMMO1DIso(type=:GT, n=dummy(beam.λ, 1.5, 0), d=0.)
+l0 = LayerTMMO1DIso(RIdb.air(beam.λ))
+l1 = LayerTMMO1DIso(RIdb.dummy(beam.λ, 1.45, 0.); type=:OT, d=1/4.)
+l2 = LayerTMMO1DIso(RIdb.dummy(beam.λ, 3.45, 0.); type=:OT, d=1/4.)
 layers = [l0, l1, l2, l1, l2, l1, l2, l1, l2, l0]
 
+# Reference wavelenth
+λ0 = 700.0
+
 # call main script
-sol = TMMO1DIsotropic(beam, layers; emfflag=true, h=10, pbgflag=true)
+sol = TMMO1DIsotropic(beam, layers; λ0=λ0, emfflag=true, h=10, pbgflag=true)
 
