6a09494068244beda96679c0bb7f39ec57278986
Leandro Acquaroli <26485264+lnacquaroli@users.noreply.github.com>
2021-05-19 15:14:14 -0400

    Update Utils.jl

--- src/Utils.jl
+++ src/Utils.jl
@@ -36,7 +36,6 @@ function Info()
 	"\n     wavelength2rgb(λ)" *
 	"\n     simple_moving_average(x)" *
 	"\n     exponential_moving_average(x)" *
-	"\n     savitzky_golay(m, n)" *
 	"\n     flatten_arrays(x)" *
 	"\n     array_arrays(x, y)" *
 	"\n     average_polarisation(pol, Xp, Xs)" *
@@ -256,67 +255,6 @@ function exponential_moving_average(y::AbstractVector{T1}; α::T1=1.0) where {T1
         y_[i] = α*y[i] + (1.0 - α)*y_[i-1]
     end
     return y_
-end
-
-"""
-
-	Implementation of the Savitzky-Golay filter of window half-width M and degree N.
-
-		y = Utils.savitzky_golay(signal, window_size, order; deriv=0, rate=1)
-			order: is the polynomial order (must be lower than window_size).
-			signal: unfiltered data vector.
-			sg: solution
-				y: filtered signal
-				params: input parameters
-				coeff: coefficients
-	
-	References:
- 	https://scipy-cookbook.readthedocs.io/items/SavitzkyGolay.html
- 	https://stackoverflow.com/a/48421852
-	https://gist.github.com/jiahao/b8b5ac328c18b7ae8a17
-
-"""
-function savitzky_golay(
-    y::AbstractVector, window_size::T0, order::T0;
-    deriv::T0=0, rate::T1=1.0,
-    ) where {T0 <: Int64, T1 <: Real}
-
-    p = _check_inputs_sg(y, window_size, order, deriv, rate)
-
-    # Determine order range and half window size
-    order_range = 0 : p.order
-    hw = Int((p.w - 1) / 2)
-
-    # Compute coefficients
-    J = [k^i for k in -hw:hw, i in order_range]
-    c = J' \ [1.0; zeros(length(order_range)-1)]
-    c .*= (p.rate)^p.deriv * factorial(p.deriv)
-
-    # Pad the signal at the extremes with values taken from the signal itself
-    initvals = p.y[1] .- abs.(reverse(p.y[2:hw+1]) .- p.y[1] )
-    endvals = p.y[end] .+ abs.(reverse(p.y[end-hw:end-1] .- p.y[end]) )
-    y_ = vcat(initvals, p.y, endvals)
-
-    return (y=_convolve_1d(y_, c), params=p, coeff=c)
-end
-
-function _check_inputs_sg(y, w, o, d, r)
-    isodd(w) || throw(ArgumentError("w must be an even number."))
-    w ≥ 1 || throw(ArgumentError("w must greater than or equal to 1."))
-    w ≥ o + 2 || throw(ArgumentError("w too small for the polynomial order chosen (w ≥ order + 2)."))
-    length(y) > 1 || throw(ArgumentError("vector x must have more than one element."))
-    return (y=Float64.(y), w=w, order=o, deriv=d, rate=Float64(r))
-end
-
-function _convolve_1d(u::Vector, v::Vector)
-    m = length(u)
-    n = length(v)
-    m > n || throw(ArgumentError("length of signal u must be greater than length of kernel v."))
-    w = zeros(m + n - 1)
-    @inbounds for j in 1:m, k in 1:n
-        w[j+k-1] += u[j]*v[k]
-    end
-    return w[n:end-n+1]
 end
 
 """
