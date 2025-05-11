99215d136d0eede5b93b2aa929a1df7a15481be5
abhro <5664668+abhro@users.noreply.github.com>
2025-05-11 17:14:44 -0400

    Put all tests in a package wide testset

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,14 +1,20 @@
 using ThinFilmsTools
+using Aqua
+using Test
 
-include("test3omega.jl")
+@testset "ThinFilmsTools" begin
+    @testset "Aqua" begin
+        Aqua.test_all(ThinFilmsTools)
+    end
+    include("test3omega.jl")
 
-include("testTMMO.jl")
+    include("testTMMO.jl")
 
-include("testFitTMMO_DBRExp.jl")
-include("testFitTMMO_LayerLLL.jl")
-include("testFitTMMO_LayerLLLSpaceSolution.jl")
-include("testFitTMMO_MC1dExp.jl")
-include("testFitBBO.jl")
+    include("testFitTMMO_DBRExp.jl")
+    include("testFitTMMO_LayerLLL.jl")
+    include("testFitTMMO_LayerLLLSpaceSolution.jl")
+    include("testFitTMMO_MC1dExp.jl")
+    include("testFitBBO.jl")
 
-include("testFitCurves.jl")
-
+    include("testFitCurves.jl")
+end
